bf0c2a251efc2d59f8548bb8bac0f5c2ca3ccbcd
Michael L Perry <michael@qedcode.com>
2014-05-10 22:53:46 -0500

    Created WCF service.

---
+++ TSODBank/BankService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TSODBank
+{
+    public class BankService : IBankService
+    {
+        public decimal GetBalance(int account)
+        {
+            return 3000.0m;
+        }
+
+        public Confirmation Withdraw(int account, decimal amount)
+        {
+            return new Confirmation
+            {
+                Identifier = "234",
+                Account = account,
+                NewBalance = 3000.0m - amount
+            };
+        }
+    }
+}
---
+++ TSODBank/IBankService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace TSODBank
+{
+    [ServiceContract]
+    public interface IBankService
+    {
+        [OperationContract]
+        decimal GetBalance(int account);
+
+        [OperationContract]
+        Confirmation Withdraw(int account, decimal amount);
+    }
+
+    [DataContract]
+    public class Confirmation
+    {
+        [DataMember]
+        public string Identifier { get; set; }
+
+        [DataMember]
+        public int Account { get; set; }
+
+        [DataMember]
+        public decimal NewBalance { get; set; }
+    }
+}
